91ba92eb67a23ddc576767f536c5753d5064be20
anibal gomez <balerocms@gmail.com>
2025-06-17 09:26:06 -0600

    remo load lang mod

--- site/apps/virtual_page/virtual_page_Controller.php
+++ site/apps/virtual_page/virtual_page_Controller.php
@@ -121,92 +121,45 @@ class virtual_page_Controller extends ControllerHandler {
 			$shield_var = $this->objSecurity->antiXSS($_GET['app']);
 			$class_methods = get_class_methods($shield_var . "_Controller");
 			//var_dump($class_methods);
-	
-				
-			$mods = new Modloader("languages");
-			$ModLangs = array();
-			$objModLangs = new mod_languages_Model();
-			$ModLangs = $objModLangs->get_lenguages();
 
 			$cfgSettings = new configSettings();
 			
 			if($cfgSettings->multilang == "yes") {
 			
-				foreach ($ModLangs as $row) {
-					if($_GET['sr'] == $row['code']) {
-							
-						try {
-							
-						/**
-						 * Multilgang posts view
-						 */
-							
-						$this->lang = $row['code'];	
-						$this->objModel->lang = $this->lang;
-						$this->objView->lang = $this->lang;
-						
-						$total_rows = $this->objModel->total_pages();
-						$limit = $this->objModel->limit();
-						$p = new Pagination($total_rows, $limit);
-						$min = $p->min();
-						
-						if(isset($_GET['id'])) {
-							$id = $this->objSecurity->antiXSS($_GET['id']);
-							$query_content = $this->objModel->get_virtual_page_by_id($id);
-							$this->objView->rows = $this->objModel->rows;
-							$md = new Markdown();
-							$this->objView->content .= "<div id=\"vp-content\">" . 
-														$md->defaultTransform($this->objView->print_virtual_page($query_content))
-														. "</div>";
-						} else {
-							throw new Exception();
-						}
-		
-						} catch (Exception $e) {
-							$msgbox = new MsgBox(_VP, _VP_DONT_EXIST);
-							$this->objView->content .= $msgbox->Show();
-						}
-						$this->objView->Render();
-							
-						//echo $row['code'];
-						die();
-							
-					}
-					
-				} // for each
-			
+
+
 			} // end if config settings
-				
+
 			foreach ($class_methods as $method_name) {
-	
+
 				if(($sr == $method_name)) {
-	
+
 					switch($sr) {
 						// llama staticamente
 						//appController::$sr();
 						//appModel::$sr();
 						//AppView::$sr();
-	
-	
+
+
 						// llamar dinamicamente
 						case $sr:
 							$var->$sr();
 							break;
-	
-	
+
+
 					} // switch
-	
+
 				}
-					
+
 			} // for each
-	
+
 		} else {
 			if((!isset($_GET['sr']))) {
 				$var->main();
 			}
 		}
 
-		
+
 	} // fin de init()
 	
 	
