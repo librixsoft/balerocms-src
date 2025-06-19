ba431dfaf58d9679cbfd6e92959ed2b64f562a22
anibal gomez <balerocms@gmail.com>
2025-06-18 18:50:36 -0600

    remove comments

--- core/boot.php
+++ core/boot.php
@@ -15,14 +15,13 @@ class boot
 
     public function autoload($class)
     {
-        // Posibles rutas donde puede estar la clase
+
         $paths = [
             LOCAL_DIR . "/core/{$class}.php",
-            APPS_DIR . "{$class}/{$class}_Controller.php",                      // ej: site/apps/virtual_page/virtual_page_Controller.php
-            MODS_DIR . "{$class}/mod_{$class}_Controller.php",                 // ej: site/apps/admin/mods/users/mod_users_Controller.php
+            APPS_DIR . "{$class}/{$class}_Controller.php",
+            MODS_DIR . "{$class}/mod_{$class}_Controller.php",
         ];
 
-        // Soporte para clases dentro de cualquier app (como hacía tu autoloader personalizado)
         $apps = scandir(APPS_DIR);
         foreach ($apps as $appName) {
             if ($appName === "." || $appName === "..") {
@@ -36,15 +35,12 @@ class boot
             }
         }
 
-        // Buscar en rutas fijas
         foreach ($paths as $path) {
             if (file_exists($path)) {
                 require_once($path);
                 return;
             }
         }
-
-        // Opcional: lanzar error si no se encuentra
-        // throw new Exception("No se pudo cargar la clase: {$class}");
+        
     }
 }
