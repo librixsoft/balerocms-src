d12a53d0101377c9227e371e3adcfd5cf9124c6b
anibal gomez <balerocms@gmail.com>
2025-09-10 15:12:04 -0600

    centralized file

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Bootstrap de Balero CMS
+ * Carga archivos críticos del framework y autoload opcional de Composer
+ */
+
+define("LOCAL_DIR", __DIR__);
+
+// Archivos críticos del framework
+require_once LOCAL_DIR . '/Framework/Core/ErrorConsole.php';
+require_once LOCAL_DIR . '/Framework/Core/Boot.php';
+require_once LOCAL_DIR . '/Framework/Static/Constant.php';
+
+// Autoload de Composer opcional
+$composerAutoload = LOCAL_DIR . '/vendor/autoload.php';
+if (file_exists($composerAutoload)) {
+    require_once $composerAutoload;
+}
--- index.php
+++ index.php
@@ -5,19 +5,6 @@ use Framework\Routing\Router;
 
 const _CORE_VERSION = "1.0";
 
-$dir = dirname(__FILE__);
-$dir = str_replace("\\", "/", $dir);
-define("LOCAL_DIR", $dir);
+require_once __DIR__ . '/bootstrap.php';
 
-require_once(LOCAL_DIR . "/Framework/Core/ErrorConsole.php");
-require_once(LOCAL_DIR . "/Framework/Core/Boot.php");
-require_once(LOCAL_DIR . "/Framework/Static/Constant.php");
-
-$autoload = __DIR__ . '/vendor/autoload.php';
-
-if (file_exists($autoload)) {
-    require $autoload;
-}
-
-(new Boot)::instantiateClass(Router::class)->initBalero();
-
+(new Boot)->instantiateClass(Router::class)->initBalero();
