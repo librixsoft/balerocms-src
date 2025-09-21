445de4c65a85138b3907384306f42ce618a5e246
anibal gomez <balerocms@gmail.com>
2025-09-21 15:15:09 -0600

    comment

--- public/assets/js/notification.js
+++ public/assets/js/notification.js
@@ -1,3 +1,8 @@
+/**
+ * Balero CMS Notification Flash Messages JS Lib
+ * @param key SESSION key
+ */
+
 function deleteMessage(key) {
     const formData = new FormData();
     formData.append('key', key);
@@ -8,7 +13,7 @@ function deleteMessage(key) {
     })
         .then(res => res.json())
         .then(data => { console.log(data.status, data.message); })
-        .catch(err => console.error('Error al borrar flash:', err));
+        .catch(err => console.error('Error deleting flash message:', err));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
