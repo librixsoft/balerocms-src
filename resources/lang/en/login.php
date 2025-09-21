84240667318b7327acb5ef79cae2bd81b4b9a36a
anibal gomez <balerocms@gmail.com>
2025-09-21 13:31:31 -0600

    i18n

---
+++ resources/lang/en/login.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'username' => 'Usr',
+    'password' => 'Pwd',
+    'submit'   => 'Log In',
+    'message'  => 'Invalid credentials',
+    'back'     => '← Back to Front-End',
+];
---
+++ resources/lang/es/login.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'username' => 'Usuario',
+    'password' => 'Contraseña',
+    'submit'   => 'Iniciar sesión',
+    'message'  => 'Credenciales inválidas',
+    'back'     => '← Volver al Front-End',
+];
