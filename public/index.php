0d19c50fb2f06fbe2a7d7a82999c37ab0828f396
anibal gomez <balerocms@gmail.com>
2025-09-22 10:26:17 -0600

    restricted content only public folder access

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ use Framework\Core\Boot;
 use Framework\Routing\Router;
 
 const _CORE_VERSION = "1.0";
-const LOCAL_DIR = __DIR__;
+const LOCAL_DIR = __DIR__ . '/..';
 
-require_once __DIR__ . '/bootstrap.php';
+require_once LOCAL_DIR . '/bootstrap.php';
 
 (new Boot)->instantiateClass(Router::class)->initBalero();
